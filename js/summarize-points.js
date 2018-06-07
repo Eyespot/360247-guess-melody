71eb132d4f83ab9326d14e62de5709287f8a17e8
Eyespot <eger_np_@inbox.ru>
2018-06-08 01:40:22 +0300

    Чистит магические значения

--- js/summarize-points.js
+++ js/summarize-points.js
@@ -1,19 +1,25 @@
-export default (playerAnswers, playerLivesLeft) => {
-  if (playerAnswers.length < 10) {
+const DEFEAT_RESULT = -1;
+const MINIMAL_ANSWERS_QUANTITY = 10;
+const CORRECT_ANSWER_POINTS = 1;
+const CORRECT_FAST_ANSWER_POINTS = 2;
+const MISTAKE_PENALTY = 2;
 
-    return -1;
+export default (playerAnswers, playerLivesLeft) => {
+  if (playerAnswers.length < MINIMAL_ANSWERS_QUANTITY) {
+
+    return DEFEAT_RESULT;
   }
 
   let points = 0;
   for (const answer of playerAnswers) {
     if (answer.isCorrect) {
-      points += answer.isFast ? 2 : 1;
+      points += answer.isFast ? CORRECT_FAST_ANSWER_POINTS : CORRECT_ANSWER_POINTS;
     } else {
-      points -= 2;
+      points -= MISTAKE_PENALTY;
       playerLivesLeft--;
     }
     if (playerLivesLeft < 1) {
-      return -1;
+      return DEFEAT_RESULT;
     }
   }
 
