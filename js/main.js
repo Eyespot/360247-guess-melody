7711eb7483a775952c38468b25addb1cab3771eb
Eyespot <eger_np_@inbox.ru>
2018-06-28 13:15:54 +0300

    чистит проект

--- js/main.js
+++ js/main.js
@@ -1,22 +1,4 @@
 import Application from "./basis/application";
-import gameData from "./data/game-data";
-
-const preloadMedia = () => {
-  const tracks = new Set();
-  const pictures = new Set();
-
-  gameData.forEach((level) => {
-    for (const option of gameData[gameData.indexOf(level)].options) {
-      if (option.hasOwnProperty(`image`)) {
-        pictures.add(option.image);
-      } else if (option.hasOwnProperty(`source`)) {
-        tracks.add(option.source);
-      }
-    }
-  });
-  console.log(tracks);
-  console.log(pictures);
-};
 
 document.addEventListener(`DOMContentLoaded`, () => {
   Application.showWelcome();
