4e0c087d0372f3c0ffba27aa834f95b08928d9b4
Eyespot <eger_np_@inbox.ru>
2018-07-02 14:11:39 +0300

    Удаляет не нужные константы

--- js/basis/sum-up.js
+++ js/basis/sum-up.js
@@ -2,10 +2,6 @@ import gameSettings from "../data/game-settings";
 import reflectResult from "./reflect-game-result";
 import {calculateMinutes, getWordDeclension, Numeral} from "./utils";
 
-const CORRECT_ANSWER_POINTS = 1;
-const CORRECT_FAST_ANSWER_POINTS = 2;
-const MISTAKE_PENALTY = 2;
-
 const getCurrentStatistics = (state) => {
   return {
     pointsReceived: state.outcome.pointsReceived,
@@ -19,12 +15,12 @@ export const summarizePoints = (state) => {
   let points = 0;
   for (const answer of state.answers) {
     if (answer.isCorrect) {
-      points += answer.isFast ? CORRECT_FAST_ANSWER_POINTS : CORRECT_ANSWER_POINTS;
+      points += answer.isFast ? gameSettings.CORRECT_FAST_ANSWER_POINTS : gameSettings.CORRECT_ANSWER_POINTS;
       if (answer.isFast) {
         state.outcome.quickPointsReceived++;
       }
     } else {
-      points -= MISTAKE_PENALTY;
+      points -= gameSettings.MISTAKE_PENALTY;
     }
   }
 
